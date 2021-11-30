d774c3765360f42fe92fca5780360fb1d9dcd446
Hisam Fahri <hisam@alphaaiauto.com>
2021-11-30 21:13:40 +0700

    feat(project): add custom google font

--- App.tsx
+++ App.tsx
@@ -10,7 +10,7 @@ export default function App(): ReactElement {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="Welcome" component={Welcome} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/auth/Welcome.tsx
+++ src/screens/auth/Welcome.tsx
@@ -1,13 +1,14 @@
 import React, { ReactElement } from 'react'
 import { Text, TouchableHighlight, View } from 'react-native'
+import Base from '../../component/container/Base'
 
 export default function Welcome({navigation}: any): ReactElement {
     return (
-        <View>
-            <Text>Welcome Page</Text>
+        <Base>
+            <Text style={{fontFamily: 'MavenPro-Black'}}>Welcome Page</Text>
             <TouchableHighlight onPress={() => navigation.navigate('Login')}>
                 <Text>Go</Text>
             </TouchableHighlight>
-        </View>
+        </Base>
     )
 }
