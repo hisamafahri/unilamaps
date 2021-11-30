698bfc438859b79d23ca90ed7409abf06dd07424
Hisam Fahri <hisam@alphaaiauto.com>
2021-11-30 21:05:36 +0700

    feat(component): add base component

---
+++ src/component/container/Base.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { KeyboardAvoidingView, Platform, ScrollView, StatusBar } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Base = ({ children }: any) => {
+    return (
+        <KeyboardAvoidingView behavior={Platform.OS == "ios" ? "padding" : "height"} style={{flex: 1}} keyboardVerticalOffset={0}>
+                <ScrollView>
+                    <SafeAreaView>
+                        <StatusBar backgroundColor="#FFFFFF" barStyle="dark-content" />
+                        {children}
+                    </SafeAreaView>
+                </ScrollView>
+        </KeyboardAvoidingView>
+    )
+}
+
+export default Base
