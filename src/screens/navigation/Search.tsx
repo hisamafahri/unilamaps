fd5933f6444e777e170fd1c83e405f3017834fcf
Hisam Fahri <hisam@alphaaiauto.com>
2021-12-18 23:01:00 +0700

    feat(search): add search header

--- src/screens/navigation/Search.tsx
+++ src/screens/navigation/Search.tsx
@@ -1,11 +1,37 @@
 import React, { ReactElement } from 'react'
-import { Text } from 'react-native'
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native'
+import ArrowIcon from '../../../assets/svg/helper/ArrowIcon'
 import Base from '../../component/container/Base'
+import { COLORS } from '../../constant/Color'
 
-export default function Search({ }: any): ReactElement {
+export default function Search({ navigation }: any): ReactElement {
     return (
         <Base>
-            <Text>Search</Text>
+            <TouchableWithoutFeedback onPress={() => { navigation.goBack() }}>
+                <View style={styles.headerContainer}>
+                    <ArrowIcon />
+                    <Text style={styles.bigText}>Cari</Text>
+                </View>
+            </TouchableWithoutFeedback>
         </Base>
     )
 }
+
+const styles = StyleSheet.create({
+    headerContainer: {
+        marginTop: 24,
+        marginHorizontal: 16,
+        paddingRight: 16,
+        display: 'flex',
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start'
+    },
+    bigText: {
+        color: COLORS.black,
+        fontFamily: 'Rubik-Bold',
+        fontSize: 36,
+        marginLeft: 24
+    },
+});
+
